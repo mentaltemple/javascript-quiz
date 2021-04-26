d7cda269718c2b39b1c037107e524c2fce031daf
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-26 16:34:36 -0600

    created html, css and js. began basic html layout. added basic css styling to header, main, h1, h2, h3 and question box, added buttons in question box. added font family.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//user clicks a start button, timer starts
+
+//user presented with a question, user answers the question
+
+//user presented with another question
+
+//when answer is incorrect, time is subtracted from the clock
+
+//when all questions are answered, or time = 0, game over
+
+//save initials and score
