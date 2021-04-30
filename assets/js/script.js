b7768ed48b4a0b4f14bb2d19e2ee31293296b528
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-29 19:24:06 -0600

    added JS functionality, including event listeners, timer, dynamically generated questions and answer buttons, eventListeners to answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,23 @@ var questionArray = [
   {
     question: "What is a boolean?",
 
-    choice: ["A True/False Statement", "wrong", "wrong", "wrong"],
+    choice: [
+      "A True/False Statement",
+      "An alien life form",
+      "A country",
+      "A ghost",
+    ],
     answer: "A True/False Statement",
   },
   {
     question: "What is a boolean?",
 
-    choice: ["A True/False Statement", "wrong", "wrong", "wrong"],
+    choice: [
+      "A True/False Statement",
+      "A space ship",
+      "An alien life form",
+      "A ghost particle",
+    ],
     answer: "A True/False Statement",
   },
   {
@@ -80,10 +90,69 @@ clickStart.addEventListener("click", function (event) {
   function questionGen() {
     for (i = 0; i < questionArray.length; i++) {
       var questTxt = document.querySelector("#questTxt");
-      var answerButtons = document.querySelector("#buttons");
       var generateQuest = document.createElement("h3");
+
+      var answerButtons = document.querySelector("#buttons");
+
+      var makeButton1 = document.createElement("button");
+      makeButton1.innerText = questionArray[i].choice[0];
+      makeButton1.setAttribute("id", "button1");
+
+      var makeButton2 = document.createElement("button");
+      makeButton2.innerText = questionArray[i].choice[1];
+      makeButton2.setAttribute("id", "button2");
+
+      var makeButton3 = document.createElement("button");
+      makeButton3.innerText = questionArray[i].choice[2];
+      makeButton3.setAttribute("id", "button3");
+
+      var makeButton4 = document.createElement("button");
+      makeButton4.innerText = questionArray[i].choice[3];
+      makeButton4.setAttribute("id", "button4");
+
       generateQuest.textContent = questionArray[i].question;
       questTxt.appendChild(generateQuest);
+
+      answerButtons.appendChild(makeButton1);
+      answerButtons.appendChild(makeButton2);
+      answerButtons.appendChild(makeButton3);
+      answerButtons.appendChild(makeButton4);
+
+      var answerClick1 = document.querySelector("#button1");
+
+      answerClick1.addEventListener("click", function (event) {
+        console.log(answerClick1);
+      });
+      var answerClick2 = document.querySelector("#button2");
+
+      answerClick2.addEventListener("click", function (event) {
+        console.log(answerClick2);
+      });
+      var answerClick3 = document.querySelector("#button3");
+
+      answerClick3.addEventListener("click", function (event) {
+        console.log(answerClick3);
+      });
+
+      var answerClick4 = document.querySelector("#button4");
+
+      answerClick4.addEventListener("click", function (event) {
+        console.log(answerClick4);
+      });
+      // pull data from button clicked
+
+      var but1txt = answerClick1.textContent;
+      var but2txt = answerClick2.textContent;
+      var but3txt = answerClick3.textContent;
+      var but4txt = answerClick4.textContent;
+
+      if (but1txt === questionArray[i].answer) {
+        console.log(but2txt);
+        console.log(questionArray[i].answer);
+
+        // ++score;
+      }
+      return;
     }
   }
 });
@@ -105,6 +174,7 @@ function setTime() {
 
 function sendMessage() {
   timeEl.textContent = "TIME'S UP!";
+  displayQbox.setAttribute("style", "display: none;");
   var imgEl = document.createElement("img");
   imgEl.setAttribute("src", "./assets/images/game-over.jpg");
   mainEl.appendChild(imgEl);
@@ -113,6 +183,12 @@ function sendMessage() {
 //hide start screen
 
 //user presented with a question, user answers the question
+
+//user answer is compared to correct answer
+
+//if correct, add to score
+
+//if incorrect, subtract from time
 
 //user presented with another question
 
